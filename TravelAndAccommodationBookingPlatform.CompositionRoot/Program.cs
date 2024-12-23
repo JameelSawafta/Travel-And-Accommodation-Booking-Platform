1290a3ad88970ed09ed9f4f438c2912639d37222
JameelSawafta <jameel.wlar999@gmail.com>
2024-12-23 16:33:14 +0200

    Init Solution

---
+++ TravelAndAccommodationBookingPlatform.CompositionRoot/Program.cs
@@ -0,0 +1,49 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TravelAndAccommodationBookingPlatform.CompositionRoot;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
+        
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        
+        builder.Services.AddApiVersioning(
+            setupAction =>
+            {
+                setupAction.ReportApiVersions = true;
+                setupAction.AssumeDefaultVersionWhenUnspecified = true;
+                setupAction.DefaultApiVersion =
+                    ApiVersion.Default; 
+                setupAction.ApiVersionReader = new HeaderApiVersionReader("x-api-version");
+            }
+        );
+        
+        var app = builder.Build();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseHttpsRedirection();
+        
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+        app.Run();
+    }
+}
