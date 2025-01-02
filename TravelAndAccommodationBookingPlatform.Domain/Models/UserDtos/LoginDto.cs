cad0bb76581c69d8403a4760faa4dee9a688380c
JameelSawafta <jameel.wlar999@gmail.com>
2025-01-02 23:58:43 +0200

    [Feat] add User Dtos

---
+++ TravelAndAccommodationBookingPlatform.Domain/Models/UserDtos/LoginDto.cs
@@ -0,0 +1,7 @@
+namespace TravelAndAccommodationBookingPlatform.Domain.Models.UserDtos;
+
+public class LoginDto
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
