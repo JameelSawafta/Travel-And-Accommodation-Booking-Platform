8ee9a1fd119e19804dd6e859dc92fd82686ff4df
JameelSawafta <jameel.wlar999@gmail.com>
2025-01-06 02:39:34 +0200

    [docs] add Authentication using JWT In Swagger UI

--- TravelAndAccommodationBookingPlatform.CompositionRoot/Program.cs
+++ TravelAndAccommodationBookingPlatform.CompositionRoot/Program.cs
@@ -53,6 +53,30 @@ public class Program
             var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
             var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
             options.IncludeXmlComments(xmlPath);
+            
+            options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                Description = "Enter 'Bearer' [space] and then your token in the text input below.<br> Example: 'Bearer 12345abcdef'"
+            });
+            options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+            {
+                {
+                    new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                    {
+                        Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                        {
+                            Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
         });
 
         builder.Services.AddDbContext<TravelAndAccommodationBookingPlatformDbContext>(
