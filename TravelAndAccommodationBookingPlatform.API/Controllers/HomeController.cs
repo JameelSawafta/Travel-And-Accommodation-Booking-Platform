e1b590256f2b941d5f3901fe045cae5cb5511502
JameelSawafta <jameel.wlar999@gmail.com>
2025-01-17 23:54:43 +0200

    [Feat] add Get Recently Visited Hotels to home controller

--- TravelAndAccommodationBookingPlatform.API/Controllers/HomeController.cs
+++ TravelAndAccommodationBookingPlatform.API/Controllers/HomeController.cs
@@ -14,24 +14,34 @@ namespace TravelAndAccommodationBookingPlatform.API.Controllers;
 public class HomeController : Controller
 {
     private readonly IHotelService _hotelService;
+    private readonly IBookingService _bookingService;
 
-    public HomeController(IHotelService hotelService)
+
+    public HomeController(IHotelService hotelService, IBookingService bookingService)
     {
         _hotelService = hotelService;
+        _bookingService = bookingService;
     }
-    
+
     [HttpGet("search")]
-    public async Task<PaginatedList<HotelSearchResultDto>> SearchHotelsAsync([FromQuery] SearchRequestDto searchRequest, int pageSize, int pageNumber)
+    public async Task<PaginatedList<HotelSearchResultDto>> SearchHotelsAsync([FromQuery] SearchRequestDto searchRequest,
+        int pageSize, int pageNumber)
     {
         var validator = new SearchHotelsValidator();
         await validator.ValidateAndThrowCustomExceptionAsync(searchRequest);
-        
+
         return await _hotelService.SearchHotelsAsync(searchRequest, pageSize, pageNumber);
     }
-    
+
     [HttpGet("featured-deals")]
     public async Task<List<FeaturedDealDto>> GetFeaturedDeals()
     {
         return await _hotelService.GetFeaturedDealsAsync(5);
+    }
+
+    [HttpGet("{userId}/recently-visited-hotels")]
+    public async Task<List<RecentlyVisitedHotelDto>> GetRecentlyVisitedHotels(Guid userId)
+    {
+        return await _bookingService.GetRecentlyVisitedHotelsAsync(userId, 5);
     }
 }
