2915d8d37d0437f1f1b6da3a7e2012f5cdbe1ade
JameelSawafta <jameel.wlar999@gmail.com>
2025-01-17 23:52:46 +0200

    [Feat] add  Get Recently Visited Hotels to booking repository

---
+++ TravelAndAccommodationBookingPlatform.Db/Repositories/BookingRepository.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using TravelAndAccommodationBookingPlatform.Db.DbContext;
+using TravelAndAccommodationBookingPlatform.Domain.Entities;
+using TravelAndAccommodationBookingPlatform.Domain.Interfaces.Repositories;
+using TravelAndAccommodationBookingPlatform.Domain.Interfaces.Services;
+
+namespace TravelAndAccommodationBookingPlatform.Db.Repositories;
+
+public class BookingRepository : IBookingRepository
+{
+    private readonly TravelAndAccommodationBookingPlatformDbContext _context;
+    private readonly IPaginationService _paginationService;
+
+    public BookingRepository(TravelAndAccommodationBookingPlatformDbContext context, IPaginationService paginationService)
+    {
+        _context = context;
+        _paginationService = paginationService;
+    }
+    
+    public async Task<List<Hotel>> GetRecentlyVisitedHotelsAsync(Guid userId, int count)
+    {
+        var hotelIds = await _context.Bookings
+            .Where(b => b.UserId == userId)
+            .OrderByDescending(b => b.CheckOutDate) 
+            .Select(b => b.Room.HotelId) 
+            .Distinct() 
+            .Take(count) 
+            .ToListAsync();
+
+        var hotels = await _context.Hotels
+            .Where(h => hotelIds.Contains(h.HotelId)) 
+            .Include(h => h.City) 
+            .Include(h => h.Rooms) 
+            .ToListAsync();
+
+        return hotels;
+    }
+}
