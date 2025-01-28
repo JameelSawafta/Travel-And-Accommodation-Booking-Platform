ebcdf39cd14cd39f748ba1718b6cf0d61568c631
JameelSawafta <jameel.wlar999@gmail.com>
2025-01-28 02:48:19 +0200

    [Feat] add cart feature to Cart Repository

---
+++ TravelAndAccommodationBookingPlatform.Domain/Interfaces/Repositories/ICartRepository.cs
@@ -0,0 +1,12 @@
+using TravelAndAccommodationBookingPlatform.Domain.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Domain.Interfaces.Repositories;
+
+public interface ICartRepository
+{
+    Task AddToCartAsync(Cart cartItem);
+    Task<(IEnumerable<Cart> Items, int TotalCount)> GetCartItemsAsync(Guid userId, int pageNumber, int pageSize);
+    Task RemoveFromCartAsync(Guid cartId);
+    Task ClearCartAsync(Guid userId);
+    Task<bool> HasDateConflictAsync(Guid userId, Guid roomId, DateTime checkInDate, DateTime checkOutDate);
+}
